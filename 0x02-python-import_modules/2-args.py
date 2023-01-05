aa8c6621209506020ff5017ae7ac14c4da7fdebd
amanuel <amanuel0924@gmail.com>
2023-01-05 22:33:41 +0300

    args

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+	from sys import argv
+	 num = len(argv)
+    if num == 1:
+        print("{} arguments.".format(num - 1))
+    elif num == 2:
+        print("{} argument:".format(num - 1))
+    else:
+        print("{} arguments:".format(num - 1))
+
+    for i in range(1, num):
+        print("{}: {}".format(i, argv[i]))
+
