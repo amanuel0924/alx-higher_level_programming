bdc07731ebfe1d8d4e5e706960699261799181bf
amanuel <amanuel0924@gmail.com>
2023-01-05 22:41:42 +0300

    args1

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-	from sys import argv
-	num = len(argv)
+    from sys import argv
+    num = len(argv)
     if num == 1:
         print("{} arguments.".format(num - 1))
     elif num == 2:
