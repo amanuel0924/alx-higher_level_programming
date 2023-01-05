da56b34c0570b2456c8f9227cfcce854e3235d81
amanuel <amanuel0924@gmail.com>
2023-01-05 23:08:38 +0300

    hidden

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import hidden_4
-	for i in dir(hidden_4):
-        if not (i[0] == '_' and i[1] == '_'):
-            print(i)
+    for s in dir(hidden_4):
+        if s[:2] != "__":
+            print("{:s}".format(s))
